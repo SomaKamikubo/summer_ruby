7321b127060b3760385cb2b54bb2c13c76a51680
e195708 <e195708@ie.u-ryukyu.ac.jp>
2021-08-19 16:40:11 +0900

    コントローラーとアクションまで

---
+++ kamikubo_app/app/controllers/frends_controller.rb
@@ -0,0 +1,7 @@
+class FrendsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+end
--- kamikubo_app/config/routes.rb
+++ kamikubo_app/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+  get 'frends/new'
+  get 'frends/show'
+  get 'items/new'
+  get 'items/show'
+  resources :friends
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
