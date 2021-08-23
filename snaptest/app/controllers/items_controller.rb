8612791d1ef5417e498b914fe0969b2f6817d05d
yuki_n0212 <yuki02129@icloud.com>
2021-08-23 15:52:49 +0900

    ralisチュートリアル一章と二章

---
+++ snaptest/app/controllers/items_controller.rb
@@ -0,0 +1,14 @@
+# config:utf-8
+
+class ItemsController < ApplicationController
+
+    def new
+    end
+
+    def show
+    end
+
+    def hello
+        render html:'hello wowfnaofj'
+    end
+end
